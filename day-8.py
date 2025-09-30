6d33ed3b62395612ae09e16fa4a9ea15577189d3
Meharban Saifi <harrysaif675@gmail.com>
2025-09-30 22:25:15 +0530

    day8

---
+++ day-8.py
@@ -0,0 +1,16 @@
+def maximumProfit():
+    n=int(input("Enter number of elements: "))  # size of array
+    arr=[]
+    max_profit=0
+    min_price=float('inf')
+    for i in range(n):
+        num=int(input(f"Enter element {i+1}: "))
+        arr.append(num)
+    print("Array is:", arr)
+    
+    for i in range(0, n-1):
+        min_price=min(min_price, arr[i])
+        profit=arr[i]-min_price
+        max_profit=max(max_profit, profit)
+    print("Maximum Profit:", max_profit)
+maximumProfit()
